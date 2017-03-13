6d8ca8d93bc70ee10deb482b1721b98a84ef3ffc
ben rudgers <ben.rudgers@gmail.com>
2017-03-12 21:26:00 -0500

    fleshed out =remove= to include the =count= parameter

--- py_lists.py
+++ py_lists.py
@@ -16,8 +16,53 @@ def identity(x):
 def copy_list(ls):
     return [i for i in ls]
 
-def remove(item, sequence, from_end=False, test=identity, test_not=False, start=False, end=False, count=False, key=identity):
-    return [i for i in sequence if not test(key(i)) == item]
+def remove(item, sequence, from_end=False, test=equal, test_not=False, start=0, end=False, count=False, key=identity):
+    # make a copy of the sequence for local mutation
+    seq = copy_list(sequence)
+    # if no end is specified, use the end of seq
+    if not end:
+        end = len(seq) - 1
+    # chop the sequence before possible reversing    
+    seq = seq[start:end]
+    # reverse the target sequence if requested
+    if from_end:
+        seq = seq.reverse()
+    # If there's no count, let it be seq length
+    if not count:
+        count = len(seq)
+
+    # Add the iterate closure
+    def iterate():
+        c = 0
+        ret = []
+        for i in seq:
+            if c < count:
+                if test(key(i),item):
+                    c += 1
+                else:
+                    ret += [i]
+            else:
+                ret += [i]
+        return ret
+    # Add iterate_not closure
+    def iterate():
+        c = 0
+        ret = []
+        for i in seq:
+            if c < count:
+                if not test(key(i),item):
+                    c += 1
+                else:
+                    ret += [i]
+            else:
+                ret += [i]
+        return ret
+
+    # Main logic
+    if test_not:
+        return iterate()
+    else:
+        return iterate_not()
 
 def remove_if(pred, seq):
     return [i for i in pred if not pred(i)]
